406786172d981b10ba19ff8c470a14029049910d
EMCarrami <eli.carrami@gmail.com>
2023-11-01 13:28:28 +0000

    localization_metrics

--- run.py
+++ run.py
@@ -7,8 +7,7 @@ from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.loggers import WandbLogger
 
 import wandb
-from cprt.data.cprt_datamodule import CprtDataModule
-from cprt.data.data_utils import load_data_from_path, random_split_df
+from cprt.data.datamodule_factory import creat_datamodule
 from cprt.model.cprt_model import Cprt
 from cprt.utils import ROOT
 
@@ -18,19 +17,7 @@ def train_cprt(config: Dict[str, Any], log_to_wandb: bool = False) -> None:
     if "seed" in config:
         seed_everything(config["seed"])
 
-    # TODO: Move to a function
-    data_dict = load_data_from_path(config["data"]["data_dict_path"])
-    data_dict = {
-        k: {"sequence": v["sequence"], "info": [i for i in v["info"] if "?" in i]} for k, v in data_dict.items()
-    }
-    data_df = load_data_from_path(config["data"]["data_df_path"])
-    data_df = data_df[data_df["uniprot_id"].isin(data_dict)]  # type: ignore[index]
-    data_df["protein_length"] = data_df["uniprot_id"].apply(lambda x: len(data_dict[x]["sequence"]))
-    data_df = data_df[data_df["protein_length"] < config["datamodule"]["max_protein_length"]]
-    data_df.reset_index(drop=True, inplace=True)
-    random_split_df(data_df, config["data"]["split_ratios"])
-
-    datamodule = CprtDataModule(data_dict, data_df, **config["datamodule"])  # type: ignore[arg-type]
+    datamodule = creat_datamodule(**config["data"], datamodule_config=config["datamodule"], only_keep_questions=True)
     model = Cprt(**config["model"])
     checkpoint_callback = ModelCheckpoint(
         monitor="loss/val_loss",

a8ba9dfbf29bfd1626daedcdb5399b58251bc6b0
EMCarrami <93011213+EMCarrami@users.noreply.github.com>
2023-11-14 14:30:40 +0000

    wte wpe

--- cprt/model/helper_modules.py
+++ cprt/model/helper_modules.py
@@ -1,4 +1,5 @@
-from typing import Any, Dict, Optional, Tuple, Union, cast
+import math
+from typing import Any, Dict, Literal, Optional, Tuple, Union, cast
 
 import torch
 from torch import Tensor, nn
@@ -189,7 +190,7 @@ class FeedForwardNetwork(nn.Module):
 class TruncatedESM2(nn.Module):
     """Truncates an existing pre-trained ESM2 model."""
 
-    def __init__(self, pretrained_model: nn.Module, layer_to_keep: int):
+    def __init__(self, pretrained_model: nn.Module, layer_to_keep: int | Literal["wte", "wpe"]):
         """
         Truncate an ESM2 model to only compute the necessary encodings.
 
@@ -198,31 +199,67 @@ class TruncatedESM2(nn.Module):
         """
         super().__init__()
         assert pretrained_model.__class__.__name__ == "ESM2"
-        self.embed_tokens = cast(nn.Module, pretrained_model.embed_tokens)
-        self.register_buffer("embed_scale", torch.tensor(pretrained_model.embed_scale))
         self.padding_idx = cast(int, pretrained_model.padding_idx)
 
-        layers = cast(nn.ModuleList, pretrained_model.layers)
-        num_layers = len(layers)
-        if layer_to_keep < 0:
-            total_layers = num_layers
-            layer_to_keep = total_layers + layer_to_keep + 1
-        assert 0 < layer_to_keep <= num_layers
-
-        self.layers = nn.ModuleList([layers[i] for i in range(layer_to_keep)])
+        if isinstance(layer_to_keep, int):
+            self.requires_transformers = True
+            self.embed_tokens = cast(nn.Module, pretrained_model.embed_tokens)
+            layers = cast(nn.ModuleList, pretrained_model.layers)
+            num_layers = len(layers)
+            if layer_to_keep < 0:
+                total_layers = num_layers
+                layer_to_keep = total_layers + layer_to_keep + 1
+            assert 0 < layer_to_keep <= num_layers
+            self.layers = nn.ModuleList([layers[i] for i in range(layer_to_keep)])
+        elif layer_to_keep == "wte":
+            self.requires_transformers = False
+            self.embed_tokens = cast(nn.Module, pretrained_model.embed_tokens)
+        elif layer_to_keep == "wpe":
+            self.requires_transformers = False
+            text_embedder = cast(nn.Module, pretrained_model.embed_tokens)
+            self.embed_tokens = nn.Sequential(
+                text_embedder, PositionalEncoding1D(cast(int, pretrained_model.embed_dim))
+            )
+        else:
+            raise ValueError(
+                f"layer_to_keep {layer_to_keep} invalid. Should be an int indicating the transformer layer number. "
+                "Alternatively, wte or wpe could be used to use aa embeddings -/+ positional embeddings, respectively."
+            )
 
     def forward(self, tokens: Tensor) -> Tensor:
         """Apply ESM2 encoding operations."""
-        x = self.embed_tokens(tokens) * self.embed_scale
-
-        padding_mask = tokens.eq(self.padding_idx)
-        x = x * (1 - padding_mask.unsqueeze(-1).type_as(x))
-
-        x = x.transpose(0, 1)
-        for layer in self.layers:
-            x, _attn = layer(x, self_attn_padding_mask=padding_mask)
-        out: Tensor = x.transpose(0, 1)
+        x = self.embed_tokens(tokens)
+        if self.requires_transformers:
+            padding_mask = tokens.eq(self.padding_idx)
+            x = x * (1 - padding_mask.unsqueeze(-1).type_as(x))
+            x = x.transpose(0, 1)
+            for layer in self.layers:
+                x, _attn = layer(x, self_attn_padding_mask=padding_mask)
+            out: Tensor = x.transpose(0, 1)
+        else:
+            out = x
         return out
+
+
+class PositionalEncoding1D(nn.Module):
+    """Cosine positional encoding."""
+
+    def __init__(self, emb_dim: int, max_len: int = 1500) -> None:
+        super(PositionalEncoding1D, self).__init__()
+        position = torch.arange(max_len).unsqueeze(1).float()
+
+        div_term = torch.exp(torch.arange(0, emb_dim, 2).float() * (-math.log(10000.0) / emb_dim))
+        pe = torch.zeros(max_len, emb_dim)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Add positional encoding to each element in the batch."""
+        x = x + self.pe[:, : x.size(1)]  # type: ignore[index]
+        return x
 
 
 # TODO: Implement double-cross-attention:
